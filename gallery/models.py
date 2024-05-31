7502e2d18881272a8dd269472d6a94c8eef0dc33
alina <alkovryga@gmail.com>
2024-05-31 09:41:13 +0300

    add models image and category

---
+++ gallery/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    categories = models.ManyToManyField(Category, related_name='images')
+    created_date = models.DateField(auto_now_add=True)
+    age_limit = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
